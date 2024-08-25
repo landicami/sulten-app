982cf2d16d81e45da988a03c0e9a4cb82172c2ea
Ann Bacharach <ann_136@hotmail.com>
2024-08-25 21:38:30 +0200

    Added logic for protected routes

--- src/components/ProtectedRoutes.tsx
+++ src/components/ProtectedRoutes.tsx
@@ -1,8 +1,14 @@
+import { Navigate } from "react-router-dom";
+import { PropsWithChildren } from "react";
+import useAuth from "../hooks/useAuth";
 
-const ProtectedRoutes = () => {
-    return (
-        <div>ProtectedRoutes</div>
-    )
-}
+const ProtectedRoutes: React.FC<PropsWithChildren> = ({ children }) => {
+	const { currentAdmin } = useAuth();
+	if (!currentAdmin) {
+		return <Navigate to="/login" />;
+	} else {
+		return children;
+	}
+};
 
 export default ProtectedRoutes;
