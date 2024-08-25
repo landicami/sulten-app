a218c5e7db4dc7a0bffff8aadc3cc7e3272f3f03
Camilla Landin <landin.camilla8@gmail.com>
2024-08-25 21:34:53 +0200

    adding basic map to mapPage with provider, map and advanced marker

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,8 +1,60 @@
+import {
+	APIProvider,
+	AdvancedMarker,
+	InfoWindow,
+	Map,
+	MapCameraChangedEvent,
+	Pin,
+	useMap,
+} from "@vis.gl/react-google-maps";
+import { useState } from "react";
+import Card from "react-bootstrap/Card";
 
 export const MapPage = () => {
-    return (
-        <div>MapPage</div>
-    );
-}
+	const [openInfo, setOpenInfo] = useState(false);
+	const point = {
+		key: "theRocks",
+		location: { lat: 55.60587, lng: 13.00073 },
+	};
+
+	return (
+		<APIProvider apiKey={import.meta.env.VITE_GOOGLE_API_KEY} onLoad={() => console.log("Maps API has loaded.")}>
+			<h1>Our map</h1>
+			<div style={{ height: "100vh", width: "50vw" }}>
+				<Map
+					defaultZoom={13}
+					defaultCenter={{ lat: 55.60587, lng: 13.00073 }} //ens geolocation
+					mapId={import.meta.env.VITE_GOOGLE_MAP_ID}
+					onCameraChanged={(ev: MapCameraChangedEvent) =>
+						console.log("camera changed:", ev.detail.center, "zoom:", ev.detail.zoom)
+					}
+				>
+					<AdvancedMarker
+						key={point.key}
+						clickable={true}
+						onClick={() => setOpenInfo(true)}
+						position={point.location}
+					>
+						<Pin background={"#FBBC04"} glyphColor={"#000"} borderColor={"#000"} />
+					</AdvancedMarker>
+
+					{openInfo && (
+						<InfoWindow
+							position={{ lat: -33.860664, lng: 151.208138 }}
+							onCloseClick={() => setOpenInfo(false)}
+						>
+							<Card>
+								<Card.Title>Restaurang</Card.Title>
+								<Card.Text>Här kommer information</Card.Text>
+								<br />
+								<br />
+							</Card>
+						</InfoWindow>
+					)}
+				</Map>
+			</div>
+		</APIProvider>
+	);
+};
 
 export default MapPage;
