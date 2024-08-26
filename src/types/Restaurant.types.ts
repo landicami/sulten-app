15daa20f377ce40fc19f3f6e102306bad1cd469b
Ann Bacharach <ann_136@hotmail.com>
2024-08-26 16:07:50 +0200

    Small changes of types

--- src/types/Restaurant.types.ts
+++ src/types/Restaurant.types.ts
@@ -14,24 +14,7 @@ export interface Restaurant {
 	website: string | null;
 	facebook: string | null;
 	instagram: string | null;
-	photoFiles: FileList;
-	longLat: LongLat;
-}
-
-export interface UpdateRestaurantFormData {
-	approvedByAdmin: boolean;
-	name: string;
-	address: string;
-	city: string;
-	description: string | null;
-	category: string;
-	offer: string[];
-	email: string | null;
-	phone: number | null;
-	website: string | null;
-	facebook: string | null;
-	instagram: string | null;
-	photoFiles: FileList;
+	/* photoFiles: FileList; */
 	longLat: LongLat;
 }
 
