3221e5f6f1fee089799646772da39e1e9acf6ea0
Camilla Landin <landin.camilla8@gmail.com>
2024-08-29 13:42:32 +0200

    Correct editpage after Linneas commit with ...restData

--- src/pages/Auth/EditRestaurantPage.tsx
+++ src/pages/Auth/EditRestaurantPage.tsx
@@ -4,35 +4,47 @@ import { useParams } from "react-router-dom";
 import { Restaurant } from "../../types/Restaurant.types";
 import RestaurantForm from "../../components/RestaurantForm";
 import useRestaurant from "../../hooks/useRestaurant";
+import { FirebaseError } from "firebase/app";
+import { toast } from "react-toastify";
 
 const EditRestaurantPage = () => {
-    const { id } = useParams();
-    const { data: resturant, loading } = useRestaurant(id);
+	const { id } = useParams();
+	const { data: resturant, loading } = useRestaurant(id);
 
-    const updateResturant = async (data: Restaurant) => {
+	const updateResturant = async (data: Restaurant) => {
+		if (!id) {
+			throw new Error("No id provided");
+			return;
+		}
+		try {
+			const docRef = doc(restaurantCol, id);
+			const { photoFiles, ...restData } = data;
 
-        if (!id) {
-            throw new Error("No id provided");
-            return;
-        }
-        try {
-            const docRef = doc(restaurantCol, id);
-
-		await updateDoc(docRef, {
-			...restData,
-		});
+			await updateDoc(docRef, {
+				...restData,
+			});
+		} catch (error) {
+			if (error instanceof FirebaseError) {
+				toast.error(error.message);
+			}
+			if (error instanceof Error) {
+				toast.error(error.message);
+			} else {
+				toast.error("Error updating resturant");
+			}
+		}
 	};
 
-    if (loading || !resturant) {
-        return <p>Loading...</p>
-    }
+	if (loading || !resturant) {
+		return <p>Loading...</p>;
+	}
 
-    return (
-        <div>
-            <h3>Edit</h3>
-            <RestaurantForm initialValues={resturant} onSave={updateResturant} />
-        </div>
-    )
-}
+	return (
+		<div>
+			<h3>Edit</h3>
+			<RestaurantForm initialValues={resturant} onSave={updateResturant} />
+		</div>
+	);
+};
 
 export default EditRestaurantPage;
