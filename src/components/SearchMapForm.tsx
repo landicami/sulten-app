20f897585ba0ef07540cd53ae8203d2f418a0b82
Camilla Landin <landin.camilla8@gmail.com>
2024-09-02 19:49:30 +0200

    Removing unused hook

--- src/components/SearchMapForm.tsx
+++ src/components/SearchMapForm.tsx
@@ -5,7 +5,7 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
-import { useSearchParams } from "react-router-dom";
+// import { useSearchParams } from "react-router-dom";
 
 interface SearchMapFormProps {
 	onCitySearch: (city: string) => void;
