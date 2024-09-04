3e17b6ab6d1722f914c4c98943071ef80d1ff268
Ann Bacharach <ann_136@hotmail.com>
2024-09-04 11:57:17 +0200

    Started logic for signup. Added a form to check for admin password to be able to sign up.

---
+++ src/components/CheckPasswordForm.tsx
@@ -0,0 +1,60 @@
+import { Alert } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+const passwordForSignup = import.meta.env.VITE_PASSWORD_FOR_SIGNUP;
+
+interface CheckPasswordFormProps {
+	onCorrectPassword: () => void;
+	onIncorrectPassword: () => void;
+}
+
+type FormDataPassword = { password: string | undefined };
+
+const CheckPasswordForm: React.FC<CheckPasswordFormProps> = ({ onCorrectPassword, onIncorrectPassword }) => {
+	const {
+		handleSubmit,
+		register,
+		formState: { errors },
+	} = useForm<FormDataPassword>();
+
+	const handlePasswordSubmit: SubmitHandler<FormDataPassword> = async (data) => {
+		if (data.password === passwordForSignup) {
+			onCorrectPassword();
+		} else {
+			onIncorrectPassword();
+		}
+	};
+
+	if (!passwordForSignup) {
+		return (
+			<Alert variant="danger">
+				<h1>Admin Password not set</h1>
+				<p>There is currently no admin password wich means no new admins can sign up</p>
+				<p>Please contact the administration</p>
+			</Alert>
+		);
+	}
+
+	return (
+		<>
+			<Form onSubmit={handleSubmit(handlePasswordSubmit)} className="mb-3">
+				<Form.Group controlId="password" className="mb-3">
+					<Form.Label>Admin Password</Form.Label>
+					<Form.Control
+						type="password"
+						{...register("password", {
+							required: "You need to enter the Admin Password!",
+						})}
+					/>
+					{errors.password && <p className="invalid">{errors.password.message || "WRONG!"}</p>}
+				</Form.Group>
+
+				<Button variant="success">Submit</Button>
+			</Form>
+		</>
+	);
+};
+
+export default CheckPasswordForm;
--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,8 +1,48 @@
+import { useState } from "react";
+import useAuth from "../hooks/useAuth";
+import { Link, useNavigate } from "react-router-dom";
+import { Card, CardBody, CardText, Col, Container, Row } from "react-bootstrap";
+import CheckPasswordForm from "../components/CheckPasswordForm";
+import { toast } from "react-toastify";
 
 const SignupPage = () => {
-    return (
-        <div>SignupPage</div>
-    )
-}
+	const [okForSignup, setOkForSignup] = useState(false);
+	const { signup } = useAuth();
+	const navigate = useNavigate();
+
+	return (
+		<Container className="py-3 center-y">
+			<Row>
+				<Col md={{ span: 6, offset: 3 }}>
+					<Card className="mb-5">
+						<CardBody>
+							<Card.Title className="mb-3">Sign Up</Card.Title>
+
+							{okForSignup ? (
+								<SignupForm onSignup={handleSignup} />
+							) : (
+								<CheckPasswordForm
+									onCorrectPassword={() => setOkForSignup(true)}
+									onIncorrectPassword={() => toast.warning("🛑 Wrong password! 🛑")}
+								/>
+							)}
+						</CardBody>
+					</Card>
+
+					<Card>
+						<CardBody>
+							<CardText className="text-center">
+								Do you already have an account? ➡️{" "}
+								<Link to="/login" className="stretched-link">
+									Log In
+								</Link>
+							</CardText>
+						</CardBody>
+					</Card>
+				</Col>
+			</Row>
+		</Container>
+	);
+};
 
 export default SignupPage;
