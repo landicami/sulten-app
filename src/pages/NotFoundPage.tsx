7bb1064f7c7563b964d8b26573579d22ca18d546
Camilla Landin <landin.camilla8@gmail.com>
2024-09-04 14:32:58 +0200

    Fixing gif on NotFound and info on AboutUs

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,16 +1,25 @@
+import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
-import { Link } from "react-router-dom";
-
+import Image from "react-bootstrap/Image";
+import { Link, useNavigate } from "react-router-dom";
 
 const NotFoundPage = () => {
-    return (
-        <Container className="notfound-wrapper">
-            <h2>This page does not exist</h2>
-            <div className="button">
-                <Link to={"/"}>&lt; Homepage</Link>
-            </div>
-        </Container>
-    )
-}
+	const navigate = useNavigate();
+	const handleClick = () => {
+		navigate("/");
+	};
+	return (
+		<Container className="notfound-wrapper">
+			<Image
+				fluid
+				src={
+					"https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExbXlrMjkyeTlpM3lzbzY2Nm1wNTNjcHdpZDBsM3M0ZW40OGZ5dW1jcCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/C21GGDOpKT6Z4VuXyn/giphy.webp"
+				}
+			></Image>
+			<h2>This page does not exist</h2>
+			<Button onClick={handleClick}>Take me home, country roads!</Button>
+		</Container>
+	);
+};
 
 export default NotFoundPage;
